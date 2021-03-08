7f6f3749ae64d8d3b0134596017d191ca23ae6b5
Raul Pichardo <raul022107@gmail.com>
2021-03-07 20:13:53 -0700

    Adding more design - Adding tags creation and deletion

--- public/javascripts/planning-backlog.js
+++ public/javascripts/planning-backlog.js
@@ -1,6 +1,13 @@
 const newWorkItem = {title: "#new-item-title"};
 const createWorkItemModal = ".createNewItemModal";
+const currentIconId = "#currentType";
+const addTagBtn = "#addTagBtn";
+const tagContainer = ".tagsContainer";
+const rmTag = ".rmTag";
 
+const tagTemplate = `<span class="badge badge-secondary"> <input type="text" name="tags" placeholder="Enter tag " class='tagNme'> <span aria-hidden="true" class="rmTag">&times;</span>  </span>`;
+
+const MAX_NUMBER_OF_TAGS = 4;
 CHANGES = {
     "status": null,
     "description": null
@@ -12,8 +19,42 @@ $(function () {
     // show the active tab in the sidebar
     showActiveTab();
 
-        // clean the modal to add an user
-        $(createWorkItemModal).on('shown.bs.modal', function (e) {
-            $(newWorkItem["title"]).trigger("focus");
-        });
+    // clean the modal to add an user
+    $(createWorkItemModal).on('shown.bs.modal', function (e) {
+        $(newWorkItem["title"]).trigger("focus");
+    });
+
+    // TODO: maybe static icons?
+    $(".btnType").on("click", function(){
+        let clickedIcon = $(this).html()
+        let currentIcon = $(currentIconId).html();
+        let temp = currentIcon;
+        // console.log(clickedIcon);
+
+        $(currentIconId).empty().html(clickedIcon);
+        $(this).empty().html(temp)
+        // console.log(currentIcon);
+    });
+
+    // Add tag
+    $(addTagBtn).on("click", function(){
+        
+
+        // get number of element
+        let childrens = ($(tagContainer).children()).length;
+
+        if (childrens <= MAX_NUMBER_OF_TAGS){
+            $(tagContainer).append(tagTemplate)
+        }else{
+            alert(`Each story cannot have more than ${MAX_NUMBER_OF_TAGS} tags`);
+        }
+    });
+
+    $(document).on("click", rmTag, function(){
+        $(this).parent().remove()
+    })
+
 });
+
+function switchContenSpan(){
+}
