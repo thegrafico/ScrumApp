3038043d95c62906459113210431b3d9508ffaef
Raul Pichardo <raul022107@gmail.com>
2022-01-07 10:53:00 -0400

    Updating npm start

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_LOCAL_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true, 
             useUnifiedTopology: true,
             useCreateIndex: true,
@@ -20,7 +20,7 @@ const connectDB = async () => {
 
 const removeDB = async () => {
     try{
-        const conn = mongoose.createConnection(process.env.MONGO_LOCAL_URI);
+        const conn = mongoose.createConnection(process.env.MONGO_URI);
         await conn.dropDatabase();
         console.log("Database removed");
     }catch(err) {
